2e33e3bb9aba12cb9f5d946e5d592f845c77f678
Yanfei Zhou <quic_zyanfei@quicinc.com>
2025-05-08 14:13:05 +0530

    sepolicy: Add permissions for Bluetooth system services
    These permissions include:
    1) Add permission to run bluetooth_manager_ext service.
    2) Allow system_server to access bluetooth property.
    
    Change-Id: I95446a71f0dcfb4b57ee4062e2aa6e004a952f6c

--- build/soong/service_fuzzer_bindings.go
+++ build/soong/service_fuzzer_bindings.go
@@ -11,6 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// Changes from Qualcomm Technologies, Inc. are provided under the following license:
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
 
 package selinux
 
@@ -223,6 +227,7 @@ var (
 		"binder_calls_stats":            EXCEPTION_NO_FUZZER,
 		"biometric":                     EXCEPTION_NO_FUZZER,
 		"bluetooth_manager":             EXCEPTION_NO_FUZZER,
+		"bluetooth_manager_ext":         EXCEPTION_NO_FUZZER,
 		"bluetooth":                     EXCEPTION_NO_FUZZER,
 		"broadcastradio":                EXCEPTION_NO_FUZZER,
 		"bugreport":                     EXCEPTION_NO_FUZZER,
